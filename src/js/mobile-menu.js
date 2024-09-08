91f0bf1b0d091ac3b438e218a37ed9d4be672c6c
kczerwienka <57370801+kczerwienka@users.noreply.github.com>
2024-09-08 22:09:41 +0200

    patch nav-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -31,7 +31,7 @@
   closeMenuBtnContact.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
